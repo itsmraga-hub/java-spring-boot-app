94ca375fd363c874d7122c6d9b0695fa5ec4bcdf
Raga <ragawilliam570@gmail.com>
2023-09-19 10:45:58 +0300

    Start spring boot project

---
+++ src/main/java/com/example/demoamigoscode/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.demoamigoscode;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository
+  extends JpaRepository<User, Integer> {
+  
+}
---
+++ src/test/java/com/example/demoamigoscode/DemoAmigoscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoamigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAmigoscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
